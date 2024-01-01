28ff2be207dafdd83f87f7ab9e3df6faf5296300
LokeshJavvaji <lokeshjavvaji94@gmail.com>
2024-01-01 20:28:47 +0530

    do it

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -3,8 +3,7 @@
 # Navigate to the application directory
 cd /var/www/html/Recruitment_Management_System
 
-# Set proper permissions for the application
-chown -R www-data:www-data .
+.
 
 # Restart the Apache web server (assuming Apache is used)
 service apache2 restart
