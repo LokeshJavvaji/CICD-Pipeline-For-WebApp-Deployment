0c60a90197461ad50c8da3b9cf9b5a6502786828
LokeshJavvaji <lokeshjavvaji94@gmail.com>
2024-01-01 20:37:43 +0530

    do

---
+++ scripts/after-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Restart the Apache web server (assuming Apache is used)
+service apache2 restart
+
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Navigate to the application directory
-cd /var/www/html/Recruitment_Management_System
-
-.
-
 # Restart the Apache web server (assuming Apache is used)
 service apache2 restart
 
