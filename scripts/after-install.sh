db35732d37ada3e96917735963c3f7489da7d1e7
LokeshJavvaji <lokeshjavvaji94@gmail.com>
2024-01-02 12:28:37 +0530

    changes

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Navigate to the application directory
+mv /tmp/db_connect.php.bkp /var/www/html/Recruitment_Management_System/admin/db_connect.php
+
+
+
 # Restart the Apache web server (assuming Apache is used)
 service apache2 restart
 
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mv  /var/www/html/Recruitment_Management_System/admin/db_connect.php /tmp/db_connect.php.bkp
 # Restart the Apache web server (assuming Apache is used)
 service apache2 restart
 
